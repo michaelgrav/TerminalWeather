17ded6d238a677e3c355a314ead772aa04007119
Michael Graves <mdgrav@protonmail.com>
2022-03-06 21:24:37 -0600

    Created base program
    Functionality to call for weather works, although it is basic and just returns the current weather.

---
+++ src/TerminalWeather.java
@@ -0,0 +1,83 @@
+import org.json.*;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.*;
+import java.util.Scanner;
+
+
+public class TerminalWeather {
+    public static void main(String[] args) throws IOException {
+        checkArgs(args);
+
+        String apikey = args[0];
+        String city = args[1];
+        String state = args[2];
+
+        String theURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+        theURL += city + "," + state + "&units=imperial&appid=" + apikey;
+
+        URL apiurl = new URL(theURL);
+        JSONObject weatherJSON = getJsonContents(apiurl);
+
+        printJSONFile(weatherJSON);
+
+        printWeatherDataToTerm(weatherJSON);
+    }
+
+    static void parseResponseCode(int rCode) {
+        switch (rCode) {
+            case 429:
+                throw new RuntimeException("Exceeded the amount of allowed requests!");
+            case 404:
+                throw new RuntimeException("City not found");
+        }
+    }
+
+    static void printJSONFile(JSONObject json) {
+        StringWriter out = new StringWriter();
+        json.write(out);
+        String jsonText = out.toString();
+        System.out.println(jsonText);
+    }
+
+    static void checkArgs(String[] arguments) {
+        if (arguments.length == 0) {
+            System.out.println("Not enough args!");
+            System.exit(-1);
+        }
+        if (arguments.length > 3) {
+            System.out.println("Too many args!");
+            System.exit(-1);
+        }
+    }
+
+    static JSONObject getJsonContents(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+
+        connection.connect();
+        int responseCode = connection.getResponseCode();
+        parseResponseCode(responseCode);
+
+        String jsonContents = "";
+        Scanner reader = new Scanner(url.openStream());
+        while (reader.hasNext()) {
+            jsonContents += reader.nextLine();
+        }
+        reader.close();
+
+        return new JSONObject(jsonContents);
+    }
+
+    static void printWeatherDataToTerm(JSONObject json) {
+        System.out.println("The current temperature is " + json.getJSONObject("main").getDouble("temp"));
+    }
+}
+
+/*
+    Future stuff
+    - ask user for api key and store it
+    - let user get weather by inputting the state and city
+
+ */
